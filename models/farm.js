7fce1e6606316be14013bfa5d01397187e8e7580
Matthew Sanner <gravityskunk@gmail.com>
2023-05-17 18:41:04 -0600

    cleaned up code organization

--- models/farm.js
+++ models/farm.js
@@ -1,59 +1,35 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
 const { Schema } = mongoose;
-//or const Schema= mongoose.schema;
-
-mongoose.connect('mongodb://localhost:27017/relationshipDemo')
-    .then(() => {
-        console.log('Mongo connection open!');
-    })
-    .catch(err => {
-        console.log('Mongo connection error!');
-        console.log(err);
-    })
-
-const productSchema = new Schema({
-    name: String,
-    price: Number,
-    season: {
-        type: String,
-        enum: ['Spring', 'Summer', 'Fall', 'Winter']
-    }
-});
 
 const farmSchema = new Schema({
-    name: String,
-    city: String,
-    products: [{ type: Schema.Types.ObjectId, ref: 'Product' }]
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name!']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required']
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
+
+//mongoose query middleware, post meaning after, according to documentation this middleware is called when we use findByIdAndDelete
+farmSchema.post('findOneAndDelete', async function (farm) {
+    if (farm.products.length) {
+        const res = await Product.deleteMany({ _id: { $in: farm.products } });
+        console.log(res);
+    }
 })
 
-const Product = mongoose.model('Product', productSchema);
 const Farm = mongoose.model('Farm', farmSchema);
 
-// Product.insertMany([
-//     { name: 'Goddess Melon', price: 4.99, season: 'Summer' },
-//     { name: 'Sugar Baby Watermelon', price: 4.99, season: 'Summer' },
-//     { name: 'Asparagus', price: 3.99, season: 'Spring' }
-// ])
-
-// const makeFarm = async () => {
-//     const farm = new Farm({ name: 'Full Belly Farms', city: 'Guinda, CA' });
-//     const melon = await Product.findOne({ name: 'Goddess Melon' });
-//     farm.products.push(melon);
-//     await farm.save();
-//     console.log(farm);
-// }
-// makeFarm();
-
-const addProduct = async () => {
-    const farm = await Farm.findOne({ name: 'Full Belly Farms' });
-    const watermelon = await Product.findOne({ name: 'Sugar Baby Watermelon' });
-    farm.products.push(watermelon);
-    await farm.save();
-    console.log(farm);
-}
-
-// addProduct();
-
-Farm.findOne({ name: 'Full Belly Farms' })
-    .populate('products')
-    .then(farm => console.log(farm));
+module.exports = Farm;
